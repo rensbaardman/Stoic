13284887d4838b965cc80e08a4e0d72aacd07ade
Rens Baardman <git@rensbaardman.nl>
2019-02-19 16:01:41 +0100

    refactor and expand functional test utilities

---
+++ test/functional/popup.test.js
@@ -0,0 +1,65 @@
+const should = require('chai').should(),
+      assert = require('chai').assert,
+      expect = require('chai').expect;
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const {firefox_options, chrome_options, get_popup_url, open_in_new_tab, safe_close_window} = require('../selenium_utils.js')
+
+
+describe('popup', function() {
+
+	this.timeout(20000);
+
+	var driver;
+	var popup_url;
+
+	before(async function() {
+		driver = await new Builder()
+			.setFirefoxOptions(firefox_options)
+			.setChromeOptions(chrome_options)
+			.build();
+		await console.log('driver built');
+		popup_url = await get_popup_url(driver);
+		await console.log('popup_url fetched');
+
+	})
+
+	afterEach(async function() {
+
+		// close all windows in a safe manner:
+		// leave on 'about:blank'-tab open, so the
+		// session stays alive.
+		const handles = await driver.getAllWindowHandles();
+		for (let handle of handles) {
+			await safe_close_window(driver, handle)
+		}
+
+		// TODO: also do things like:
+		// reset local storage
+	})
+
+	after(async function() {
+		await driver.quit()
+	})
+
+
+	it("should have 'Stoic' in the title", async function() {
+
+		await driver.get(popup_url);
+
+		let header = await driver.findElement(By.css('h1'));
+		let text = await header.getText();
+
+		expect(text).to.equal('Stoic')
+
+	});
+
+	it("should show the url of the active page", async function() {
+		
+		await driver.get('http://google.com');
+		await open_in_new_tab(driver, popup_url);
+
+		await driver.sleep(5000);
+
+	})
+
+})
--- test/selenium_utils.js
+++ test/selenium_utils.js
@@ -4,7 +4,7 @@ const {By} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const chrome = require('selenium-webdriver/chrome');
 
-const package = require('../../package.json');
+const package = require('../package.json');
 const name = package.name
 const version = package.version
 
@@ -50,14 +50,18 @@ async function get_extension_id(driver) {
 	let browser_name = await get_browser_name(driver);
 
 	if (browser_name === 'firefox') {
-		await driver.get('about:debugging#addons')
+		await console.log('its firefox')
+		await open_in_new_tab(driver, 'about:debugging#addons');
+		await console.log('opened in new tab')
 		// TODO: the manifest ID Stoic@rensbaardman.nl gets injected in build_manifest() in webpack.config.js;
 		// try to get that one out (or at least move to manifest.json or something like APP_INFO)
 		let element = await driver.findElement(By.css('li[data-addon-id="Stoic@rensbaardman.nl"] dd.internal-uuid span:first-child'))
-		return element.getAttribute('innerHTML')
+		let id = await element.getAttribute('innerHTML');
+		await safe_close_current_window(driver);
+		return id;
 	} else if (browser_name === 'chrome') {
 	
-		await driver.get('chrome://extensions/')
+		await open_in_new_tab(driver, 'chrome://extensions/')
 
 		let extensions_manager    = await driver.findElement(By.css('extensions-manager'));
 		let extensions_manager_SR = await get_shadow_root(driver, extensions_manager);
@@ -73,7 +77,9 @@ async function get_extension_id(driver) {
 			let ext_name = await name_element.getText()
 
 			if (ext_name === name) {
-				return extension.getAttribute('id')
+				let id = await extension.getAttribute('id');
+				await safe_close_current_window(driver);
+				return extension.getAttribute('id');
 			}
 		}
 	} else {
@@ -94,23 +100,76 @@ async function get_extension_base_url(driver, id) {
 	}
 }
 
-async function open_popup_page(driver) {
-
+async function get_popup_url(driver) {
 	let id = await get_extension_id(driver)
 	let base_url = await get_extension_base_url(driver, id)
 	let url = `${base_url}/popup/popup.html`
-	return driver.get(url)
-
+	return url	
 }
 
+// we could also get this from the environment variable
+// SELENIUM_BROWSER
 async function get_browser_name(driver) {
 	let capabilities = await driver.getCapabilities()
 	return capabilities.get('browserName')
+}
+
+// TODO: this could be done cleaner with sending 'COMMAND/CONTROL + t', but apparently doesn't work. See https://stackoverflow.com/a/41974917/7770056
+// or checkout actions: https://club.ministryoftesting.com/t/how-to-open-new-tab-in-chrome-and-firefox/12569/2
+async function open_in_new_tab(driver, url) {
+	await driver.executeScript('window.open("about:blank");');
+	const handles = await driver.getAllWindowHandles();
+	const new_window_handle = handles[handles.length - 1];
+	await driver.switchTo().window(new_window_handle);
+	return driver.get(url);
+}
+
+async function safe_close_window(driver, handle) {
+	// closes window and shifts focus
+	// to previous in list,
+	// or if it is the last, changes it to about:blank
+	// to prevent session from closing
+
+	const handles = await driver.getAllWindowHandles();
+	const index = handles.indexOf(handle);
+	if (index === -1) {
+		throw new Error('handle does not belong to existing window')
+	} else if (index === 0) {
+		// this means it is the most left window,
+		// then change to window at the right from it
+		// (note that we have to make sure it exists!)
+		var next_focus_index = 1;
+	}
+	else {
+		var next_focus_index = index - 1;
+	}
+
+	if (handles.length === 1) {
+		// only 1 window open
+
+		// cannot assume that this window is already active
+		await driver.switchTo().window(handle);
+		return driver.get('about:blank');
+	}
+	else {
+		await driver.switchTo().window(handle);
+		await driver.close();
+		return driver.switchTo().window(handles[next_focus_index]); 
+	}
+
+}
+
+async function safe_close_current_window(driver) {
+	const current_handle = await driver.getWindowHandle();
+	return safe_close_window(driver, current_handle)
 }
 
 
 module.exports = {
 	firefox_options: firefox_options,
 	chrome_options: chrome_options,
-	open_popup_page: open_popup_page
+	get_popup_url: get_popup_url,
+	get_browser_name: get_browser_name,
+	open_in_new_tab: open_in_new_tab,
+	safe_close_window: safe_close_window
 }
