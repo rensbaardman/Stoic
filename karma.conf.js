5b5540b4d6838632c0f3ff5638328d81c696689a
Rens Baardman <git@rensbaardman.nl>
2019-06-14 01:09:38 +0200

    Cleanup karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,65 +1,46 @@
 // Karma configuration for unit testing with real browsers
+// (npm run test:unit:browser)
 
 const RewireWebpackPlugin = require('rewire-webpack-plugin');
 
 module.exports = function(config) {
-  config.set({
 
-    frameworks: ['mocha'],
+	config.set({
 
-    files: [
-      'test/unit/*.test.js'
-    ],
+		frameworks: ['mocha'],
+		reporters: ['mocha'],
 
-    preprocessors: {
-      // add webpack as preprocessor
-      'test/unit/*.test.js': [ 'webpack' ]
-    },
+		files: [
+			'test/unit/*.test.js'
+		],
 
-    webpack: {
-        mode: "development",
-        plugins: [new RewireWebpackPlugin()]
-    },
+		preprocessors: {
+			'test/unit/*.test.js': [ 'webpack' ]
+		},
 
-    reporters: ['mocha'],
+		webpack: {
+			mode: "development",
+			plugins: [new RewireWebpackPlugin()]
+		},
 
+		colors: true,
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
+		autoWatch: false,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		browsers: ['Firefox', 'Chrome'],
+		// more browsers: 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'
 
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: true,
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	})
 
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
-    // browsers: ['Firefox', 'Chrome', 'ChromeCanary', 'VirtualBoxEdge14'],
-    // to use the Microsoft Edge Virtual Box, make sure to install the official virtual machine first, and add a launcher to customLaunchers. See https://www.npmjs.com/package/karma-virtualbox-edge-launcher.
-    // alternatively on Windows: use the regular Edge karma launcher (karma-edge-launcher) (TODO: configure this automatically?)
-    // possibly also: use Chrome Puppeteer for quicker headless testing? (https://github.com/GoogleChrome/puppeteer, see karma-puppeteer-launcher). Or JSDom
-    // also: Opera? (karma-opera-launcher) VIvaldi? (karma-vivaldi-launcher) Brave? (karma-brave-launcher)
-    // more browsers: 'FirefoxDeveloper', 'FirefoxAurora', 'FirefoxNightly'
-
-    // for extensions: use customlaunchers! 
-    // see e.g. https://github.com/karma-runner/karma-firefox-launcher#loading-firefox-extensions
-    // also check: https://github.com/karma-runner/karma-firefox-launcher/issues/89
-
-    // possibly use karma-detect-browsers:  https://www.npmjs.com/package/karma-detect-browsers
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
 }
