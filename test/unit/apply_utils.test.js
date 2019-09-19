237e0294a9f7a7b7c43efc914c3f89b9ea58065e
Rens Baardman <git@rensbaardman.nl>
2019-09-19 23:18:56 +0200

    add start with completing unit tests for apply_utils

--- test/unit/apply_utils.test.js
+++ test/unit/apply_utils.test.js
@@ -394,7 +394,247 @@ describe('apply_utils', function() {
 
 		})
 
+		it('does nothing on a category change when _status is false', () => {
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyCategoryChange_spy = sinon.spy()
+			apply_utils.__set__('applyCategoryChange', applyCategoryChange_spy)
+
+			const oldSettings = {
+				_status: false
+			}
+			const newSettings = {
+				_status: false,
+				_categories: {
+					related: false
+				}
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+			assert.notCalled(applyCategoryChange_spy)
+
+		})
+
+		it('handles a rule change', () => {
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {}
+			const newSettings = {
+				'my-favorite-rule': false
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.calledOnce(applyRuleChange_spy)
+			assert.calledWith(applyRuleChange_spy, 'my host', newSettings, 'my-favorite-rule', {old: undefined, new: false})
+		})
+
+		it('does nothing on a rule change to true from undefined', () => {
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {
+			}
+			const newSettings = {
+				'hide-related-element': true
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.notCalled(applyRuleChange_spy)
+
+		})
+
+		it('does nothing on a rule change when _status is false', () => {
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {
+				_status: false
+			}
+			const newSettings = {
+				_status: false,
+				'my-favorite-rule': false
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.notCalled(applyRuleChange_spy)
+
+		})
+
+		it('does nothing on a rule change to true when cat status is false', () => {
+
+			// TODO: link cat and rule!
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {
+				_categories: {
+					'related': false
+				}
+			}
+			const newSettings = {
+				_categories: {
+					'related': false
+				},
+				'hide-related-element': true
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.notCalled(applyRuleChange_spy)
+
+		})
+
+		it('does nothing on a rule change to false when cat status is false', () => {
+
+			// TODO: link cat and rule!
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {
+				_categories: {
+					'related': false
+				}
+			}
+			const newSettings = {
+				_categories: {
+					'related': false
+				},
+				'hide-related-element': false
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.notCalled(applyRuleChange_spy)
+
+		})
+
+		it('handles a rule change to false when cat status is true (override)', () => {
+
+			// TODO: link cat and rule!
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {
+				_categories: {
+					'related': true
+				}
+			}
+			const newSettings = {
+				_categories: {
+					'related': true
+				},
+				'hide-related-element': false
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.calledOnce(applyRuleChange_spy)
+			assert.calledWith(applyRuleChange_spy, 'my host', newSettings, 'hide-related-element', {old: undefined, new: false})
+		})
+
+		it('handles a rule change to true when cat status is true (sanity check)', () => {
+
+			// TODO: link cat and rule!
+
+			const applyChanges = apply_utils.__get__('applyChanges');
+			const applyRuleChange_spy = sinon.spy()
+			apply_utils.__set__('applyRuleChange', applyRuleChange_spy)
+
+			const oldSettings = {
+				_categories: {
+					'related': true
+				},
+				'hide-related-element': false
+			}
+			const newSettings = {
+				_categories: {
+					'related': true
+				},
+				'hide-related-element': true
+			}
+
+			applyChanges('my host', oldSettings, newSettings)
+
+			assert.calledOnce(applyRuleChange_spy)
+			assert.calledWith(applyRuleChange_spy, 'my host', newSettings, 'hide-related-element', {old: false, new: true})
+		})
+
 	})
 
+	describe.only('applyStatusChange', () => {
+
+		it('removes all rules when new status is false', () => {
+
+			const applyStatusChange = apply_utils.__get__('applyStatusChange')
+
+			const getConfigMock = sinon.stub()
+			const categoriesMock = sinon.stub()
+			getConfigMock.resolves({categories: categoriesMock})
+			apply_utils.__set__('getConfig', getConfigMock)
+
+			const browserTabsQueryMock = sinon.stub()
+			browserTabsQueryMock.resolves([{id: 1234}])
+
+			// somehow I can't get browser_stub to work here...
+			const browserStub = { tabs: {
+				query: browserTabsQueryMock  }
+			}
+
+			apply_utils.__set__('browser', browserStub)
+
+			const host = 'my-host.com'
+			const tabId = '1234'
+
+			const newSettings = {
+				_status: false
+			}
+			const status = {
+				old: undefined,
+				new: false
+			}
+
+			let removeAllSpy = sinon.spy();
+			apply_utils.__set__('removeAll', removeAllSpy)
+
+			applyStatusChange(host, newSettings, status)
+
+			assert.calledOnce(removeAllSpy)
+			assert.calledWith(removeAllSpy, categoriesMock, tabId, force=true)
+
+		})
+
+	})
+
+	describe('applyCategoryChange', () => {
+
+		it('', () => {
+			assert.fail('TODO')
+		})
+
+	})
+
+	describe('applyRuleChange', () => {
+
+		it('', () => {
+			assert.fail('TODO')
+		})
+
+	})
 
 })
